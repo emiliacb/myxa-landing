099c33b341158c75be2d707ff52cb192b5e7e9f6
Emilia <66620157+emiliacb@users.noreply.github.com>
2024-03-20 17:04:14 -0300

    updated template

--- src/pages/api/send.js
+++ src/pages/api/send.js
@@ -12,17 +12,21 @@ export default async (req, res) => {
       cc: "info@drtb.com.ar",
       subject: `DRTB - Pedido de contacto - ${date}`,
       html: `
-        <div>
-            <img width="200" src="https://drtb.com.ar/logo.png">
-            <div style="display: flex;">
-                <div>
-                    <p style="font-size: 20px;">Una persona llenó el formulario para ser contactada.</p>
-                    <p style="font-size: 16px;">Fecha y Hora: ${date}</p>
-                    <p style="font-size: 16px;">Número: ${phone}</p>
-                </div>
-                <img width="100" height="80" style="margin-left: 32px; object-fit: cover" src="https://drtb.com.ar/phone.png">
+      <div style="display: grid; place-content: center; width: 80%; min-height: 90vh; padding: 16px; background-color: lightgray; margin: 0">
+        <div style="border-radius: 6px; box-shadow: 2px 2px 5px rgba(0,0,0,0.2); padding: 32px; margin: auto; width: fit-content; height: fit-content; max-width: 500px; background-color: white; border: 1px solid rgba(0,0,0,0.2)">
+          <img width="200" style="margin-left: -16px" src="https://drtb.com.ar/logo.png">
+          <div style="display: flex; flex-wrap: wrap">
+            <div>
+              <p style="font-size: 20px;">Una persona llenó el formulario para ser contactada.</p>
+              <p style="font-size: 16px;">Fecha y Hora: ${date}</p>
+              <p style="font-size: 16px;">Número: ${phone}</p>
             </div>
+            <div style="min-width: 150px; width: 50%; max-width: 50vw; border: 1px solid red; display: grid; place-content: center;">
+              <img width="50" style="width: 100%; object-fit: cover;" src="https://drtb.com.ar/phone.png">
+            </div>
+          </div>
         </div>
+      </div>
       `,
     });
 
