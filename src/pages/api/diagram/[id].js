d633bf2829c3a3cb759e82c50d3799bace92e001
em3c2 <bramasca@gmail.com>
2024-05-17 19:07:28 -0300

    diagrams implementation

--- src/pages/api/diagram/[id].js
+++ src/pages/api/diagram/[id].js
@@ -1,12 +1,17 @@
+import fetch from "node-fetch";
+
+const DIAGRAMS_CSV_URL = process?.env?.DIAGRAMS_CSV_URL || "";
+
 export default async (req, res) => {
   try {
     const { id } = req.query;
 
-    const response = await fetch(process.env.DIAGRAMS_CSV_URL)
-    const data = await response.text()
+    const response = await fetch(DIAGRAMS_CSV_URL);
+    const data = await response.text();
 
     const rows = data.split("\r");
     rows.shift();
+
     const diagrams = rows.map((r) => {
       const cells = r.split("\t");
       return {
@@ -19,19 +24,32 @@ export default async (req, res) => {
       };
     });
 
-    const selectedDiagram = diagrams.find(d => d.id === id);
+    const selectedDiagram = diagrams.find((d) => d.id === id);
 
     if (!selectedDiagram) {
-      return res.status(404).json({ error: 'No encontramos el unifilar, por favor contactanos en www.myxa.com.ar' });
+      return res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Error</title>
+        </head>
+        <body style="display: flex; flex-direction: column; width: 100%; padding: 3rem; justify-items: center; align-items: center;">
+          <div>
+            <h1>🚨 Error al encontrar el unifilar</h1>
+            <p>No encontramos el unifilar que está buscando, comuníquese con nosotros en nuestra página.</p>
+            <a href="https://www.myxa.com.ar">www.myxa.com.ar</a>
+          </div>
+        </body>
+        </html>
+      `);
     }
 
-    if (error) {
-      console.log(error);
-      return res.status(500).json({ error });
-    }
-
-    return res.redirect(selectedDiagram.publicUrl);
+    return res.redirect(selectedDiagram?.fileUrl);
   } catch (error) {
+    console.error({ error });
     return res.status(500).json({ error });
   }
 };
