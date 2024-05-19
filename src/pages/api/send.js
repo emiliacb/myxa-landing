4a40ecc24bbc12b8d4e11f25adb9f8be86726151
Emilia <66620157+emiliacb@users.noreply.github.com>
2024-05-19 19:39:23 -0300

    updated email from

--- src/pages/api/send.js
+++ src/pages/api/send.js
@@ -7,7 +7,7 @@ export default async (req, res) => {
     const { phone, date } = req.query;
 
     const { data, error } = await resend.emails.send({
-      from: "info@drtb.com.ar",
+      from: "info@myxa.com.ar",
       to: "cabralmarce@hotmail.com",
       cc: "info@myxa.com.ar",
       subject: `MYXA - Pedido de contacto - ${date}`,
